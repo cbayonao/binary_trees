30c221e9aa69c8adee133401bc9fd8c649bef179
AdrianCHdz <1214@holbertonschool.com>
2020-03-10 15:17:57 +0000

    [Feture]: Adding file that prints the binary tree in preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,5 +7,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (tree)
+    {
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+    }
 }
