bd1f438eeaeed7c186cf170f2bcc93b70c7e87fc
AdrianCHdz <1214@holbertonschool.com>
2020-03-10 23:20:52 +0000

    [Feature]: adding files

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,17 @@
 #include "binary_trees.h"
 /**
  * binary_tree_size - function that measures the size of a binary tree.
- *@tree: is a pointer to the root node of the tree to measure the size.
+ * @tree: is a pointer to the root node of the tree to measure the size.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+    int node1 = 0, node2 = 0;
 
+    if (!tree)
+	return (0);
+
+    node1 = binary_tree_size(tree->left);
+    node2 = binary_tree_size(tree->right);
+    node1 += node2;
+    return (node1 + 1);
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,5 +6,16 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-
+    size_t leaves = 0;
+    if (!tree)
+	return (0);
+    
+    if (tree->left || tree->right)
+    {
+	leaves = binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+    }
+    if (!tree->left && !tree->right)
+	return(leaves + 1);
+    return(leaves);
 }
