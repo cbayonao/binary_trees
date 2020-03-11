8028569ba93cfd1d37a36e23fd446646906c5cb3
cbayonao <drcamilob@live.com>
2020-03-11 16:21:49 -0400

    [Fix] Adding parent condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,18 +14,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (!node)
 		return (NULL);
-	if (!parent)
-		return (NULL);
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
 	if (parent->left != NULL)
 	{
-		node->parent = parent;
-		parent->left->parent = node;
 		node->left = parent->left;
+		parent->left = node;
+		node->left->parent = node;
 	}
-	parent->left = node;
+	else
+		parent->left = node;
 	return (node);
 }
