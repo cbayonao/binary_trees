fd0c40196d914880d3312efa9ef7f1fb1e9fb411
cbayonao <drcamilob@live.com>
2020-03-10 20:49:17 -0400

    [Feature] 2, 3, 4, 5, 11, 12

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,6 +9,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return(0);
 	else
 	{
-		return(sizeof(tree->left) + 1 + sizeof(tree->right));
+		return(binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 	}
 }
