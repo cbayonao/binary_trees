566c53f62dce832ef45805b718f19e3e115d698a
cbayonao <drcamilob@live.com>
2020-03-12 06:27:37 -0400

    [Feature] Reset hard

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 /**
- *binary_tree_is_full - Function that checks if a binary
+ * binary_tree_is_full - Function that checks if a binary
  * tree is full.
  *@tree: is a pointer to the root node of the tree to check.
  * Return: 1 if is full or 0 is not full or tree is NULL.
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
