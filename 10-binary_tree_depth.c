4d4450fb9d5db3bb6215bbb668e34afc59a43057
cbayonao <drcamilob@live.com>
2020-03-12 00:29:59 -0400

    [Fix] betty betty style, and tree = tree->parent

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,10 +7,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
+        int depth = 0;
 
 	if (!tree->parent || !tree)
 		return (0);
 	depth = binary_tree_depth(tree->parent);
+	tree = tree->parent;
 	return (depth + 1);
 }
