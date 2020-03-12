c4669cd438b089068687501ed3ccf5ae6270fc08
cbayonao <drcamilob@live.com>
2020-03-12 05:14:39 -0400

    [Feature] 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,21 @@
 #include "9-binary_tree_height.c"
 #include "12-binary_tree_leaves.c"
+#include "13-binary_tree_nodes.c"
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ *@x: integer.
+ *@y: integer.
+ * Return: Power between two numbers
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else if (y < 0)
+		return (-1);
+	else
+		return (1);
+}
 /**
  *binary_tree_is_perfect - Function that checks if a binary
  * tree is perfect.
@@ -10,7 +26,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if ((binary_tree_height(tree) * 2) == (binary_tree_leaves(tree)))
+	if ((_pow_recursion(2, binary_tree_height(tree))) == (int)binary_tree_leaves(tree))
 		return (1);
-	return (0);
+	else
+		return (0);
 }
