dc0fc48c570620a30cd0bdb5fc8b3c1e7acc0af0
cbayonao <drcamilob@live.com>
2020-03-12 04:06:38 -0400

    [Feature] Check if binary tree is perfect or not

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,9 +1,16 @@
-#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+#include "12-binary_tree_leaves.c"
 /**
- * binary_tree_is_perfect - function that checks if a binary tree is perfect.
+ *binary_tree_is_full - Function that checks if a binary
+ * tree is full.
  *@tree: is a pointer to the root node of the tree to check.
+ * Return: 1 if is full or 0 is not full or tree is NULL.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-
+	if (!tree)
+		return (0);
+	if ((binary_tree_height(tree) * 2) == (binary_tree_leaves(tree)))
+		return (1);
+	return (0);
 }
