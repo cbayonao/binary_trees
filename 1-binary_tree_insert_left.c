fe374f326ad9503a182ba98bebbef96b99f021d9
cbayonao <drcamilob@live.com>
2020-03-11 20:15:14 -0400

    dfgf

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,17 +10,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = NULL;
-
-	if (!parent)
-		return (NULL);
-	node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
+
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
+
+	if (!parent)
+	{
+		free(node);
+		return (NULL);
+	}
+
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
