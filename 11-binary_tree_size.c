71762ed7050743b02a9969c35aad5164f00b5546
cbayonao <drcamilob@live.com>
2020-03-12 01:36:26 -0400

    [Fix] betty betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,11 +2,12 @@
 /**
  * binary_tree_size - function that measures the size of a binary tree.
  * @tree: is a pointer to the root node of the tree to measure the size.
+ * Return: Size or 0 if tree is NULL.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree==NULL)
-		return(0);
+	if (tree == NULL)
+		return (0);
 	else
-		return(binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
