bddbe49f32686da93f20719b8a3095739ea7aae4
cbayonao <drcamilob@live.com>
2020-03-12 06:29:26 -0400

    [Feature] Reset hard

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,10 +8,12 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
 	if (!tree)
 		return (0);
-	if ((binary_tree_height(tree) * 2) == binary_tree_leaves(tree))
+	if (!tree->left && !tree->right)
 		return (1);
+	if (tree->left && tree->right)
+		return ((binary_tree_is_full(tree->left) &&
+			 (binary_tree_is_full(tree->right))));
 	return (0);
 }
